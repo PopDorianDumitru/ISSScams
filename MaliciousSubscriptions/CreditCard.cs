f01195095c6e52c96ea8342a227e4253b4f56724
Cristi <cristipopan1262@gmail.com>
2024-04-24 21:20:13 +0300

    Domain

---
+++ MaliciousSubscriptions/CreditCard.cs
@@ -0,0 +1,28 @@
+﻿using ISSProject.MaliciousSubscriptionsBackend.Domain;
+
+namespace MaliciousSubscriptions
+{
+    [TestClass]
+    public class CreditCardTests
+    {
+        [TestMethod]
+        public void Constructor_ValidArguments_ShouldInitializeCorrectly()
+        {
+            int expectedId = 1;
+            int expectedUserId = 123;
+            string expectedCreditCardHolder = "John Doe";
+            string expectedCreditCardNumber = "1234567890123456";
+            string expectedExpirationDate = "12/25";
+            string expectedCVV = "123";
+
+            var creditCard = new CreditCard(expectedId, expectedUserId, expectedCreditCardHolder, expectedCreditCardNumber, expectedExpirationDate, expectedCVV);
+
+            Assert.AreEqual(expectedId, creditCard.GetId());
+            Assert.AreEqual(expectedUserId, creditCard.GetUserID());
+            Assert.AreEqual(expectedCreditCardHolder, creditCard.GetCreditCardHolder());
+            Assert.AreEqual(expectedCreditCardNumber, creditCard.GetCreditCardNumber());
+            Assert.AreEqual(expectedExpirationDate, creditCard.GetExpirationDate());
+            Assert.AreEqual(expectedCVV, creditCard.GetCVV());
+        }
+    }
+}
---
+++ MaliciousSubscriptions/SeverePostData.cs
@@ -0,0 +1,25 @@
+﻿
+using ISSProject.MaliciousSubscriptionsBackend.Domain;
+
+namespace MaliciousSubscriptionsDomain
+{
+    [TestClass]
+    public class SeverePostDataTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            string expectedCreditCardHolder = "John Doe";
+            string expectedCreditCardNumber = "1234567890123456";
+            string expectedExpirationDate = "12/25";
+            string expectedCVV = "123";
+
+            var severePostData = new SeverePostData(expectedCreditCardHolder, expectedCreditCardNumber, expectedExpirationDate, expectedCVV);
+
+            Assert.AreEqual(expectedCreditCardHolder, severePostData.CreditCardHolder);
+            Assert.AreEqual(expectedCreditCardNumber, severePostData.CreditCardNumber);
+            Assert.AreEqual(expectedExpirationDate, severePostData.ExpirationDate);
+            Assert.AreEqual(expectedCVV, severePostData.CVV);
+        }
+    }
+}
