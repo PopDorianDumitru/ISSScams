a13dbae037881f8eb05c33538f1cbd114b031f07
MARIO-VIOREL RUS <mariorusv@yahoo.com>
2024-04-25 15:20:13 +0300

    Fake user in memory

---
+++ ISSProject-Regenerated/ScamBots/Repository/FakeUserInMemoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISSProject.Common.Mock;
+using ISSProject.ScamBots;
+
+namespace ISSProject_Regenerated.ScamBots.Repository
+{
+    internal class FakeUserInMemoryRepository : IFakeUserRepository
+    {
+
+        private readonly List<MockUser> mockUsers = new List<MockUser>();
+        public IEnumerable<MockUser> All()
+        {
+            return mockUsers.ToArray();
+        }
+
+        public MockUser ById(int id)
+        {
+            return mockUsers.Find(user => user.Id == id);
+        }
+
+        public bool Delete(MockUser entity)
+        {
+            return mockUsers.Remove(entity);
+        }
+
+        public bool Insert(MockUser entity)
+        {
+            mockUsers.Add(entity);
+            return true;
+        }
+
+        public int NumberOfBannedFakeAccounts()
+        {
+            return 13;
+        }
+
+        public int Size()
+        {
+           return mockUsers.Count;
+        }
+
+        public bool Update(MockUser entity)
+        {
+            for (int i = 0; i < mockUsers.Count; i++)
+            {
+                if (mockUsers[i].Id == entity.Id)
+                {
+                    mockUsers[i] = entity;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int UserIdByEmail(string email)
+        {
+            foreach (var user in mockUsers)
+            {
+                if (user.Email == email)
+                {
+                    return user.Id;
+                }
+            }
+            return -1;
+        }
+    }
+}
