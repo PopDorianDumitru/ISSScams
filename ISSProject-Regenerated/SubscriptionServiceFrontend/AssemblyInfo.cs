e54006817ae3b3d866f06f19808d108443a183fc
dorianpop1512@gmail.com <uratenie50@gmail.com>
2024-04-25 14:37:24 +0300

    Changed TestMockUser

--- ISSProject-Regenerated/SubscriptionServiceFrontend/AssemblyInfo.cs
+++ ISSProject-Regenerated/SubscriptionServiceFrontend/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Windows;
                                               // (used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
     InternalsVisibleTo("ISSProject-Regenerated"),
-    InternalsVisibleTo("MaliciousSubscriptionsDomain")
+    InternalsVisibleTo("MaliciousSubscriptionsDomain"),
                                               // app, or any theme specific resource dictionaries)
     InternalsVisibleTo("TestGraphAnalyzer"),
-    InternalsVisibleTo("TestCommon")
+    InternalsVisibleTo("TestCommon"),
     InternalsVisibleTo("TestSubscriptionService")
 ]
